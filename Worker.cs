e2d76b8fafef88a2396923fd6343b7cb01dc401e
Scrumlord123 <94675757+Scrumlord123@users.noreply.github.com>
2023-03-16 21:37:07 +0100

    Consumer

---
+++ Worker.cs
@@ -0,0 +1,73 @@
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Newtonsoft.Json;
+
+namespace ServiceWorker;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    // POCO klasse format til beskeder
+    private class PlanningMessage
+    {
+        public int BookingId { get; set; }
+        public int PartySize { get; set; }
+        public string PickupLocation { get; set; }
+        public Datetime PickupTime { get; set; }
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "Planning Service",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+        
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+
+            // Deserialize beskeden
+            var planningMessage = JsonConvert.DeserializeObject<PlanningMessage>(message);
+
+            //Bruger data fra POCO klasse til håndterbar data
+            Console.WriteLine(" [x] Received Planning Message: {0} - {1} (Due Date: {2})",
+                planningMessage.BookingId,
+                planningMessage.PartySize,
+                planningMessage.PickupLocation,
+                planningMessage.PickupTime);
+
+        };
+
+        channel.BasicConsume(queue: "Planning Service",
+                             autoAck: true,
+                             consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
