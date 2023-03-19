4eb1c1e91662c3f608b5bdcde745419e6b92b317
Sebastian Jensen <sohjau@gmail.com>
2023-03-19 20:45:40 +0100

    Docker compose

--- ServiceWorker/Worker.cs
+++ ServiceWorker/Worker.cs
@@ -12,14 +12,18 @@ public class Worker : BackgroundService
     private readonly ILogger<Worker> _logger;
     // Tom variabel til at holde stien til filen
     private readonly string? _docPath;
+    private readonly string? _rabbitMQ;
 
     public Worker(ILogger<Worker> logger, IConfiguration config)
     {
         // Tager Miljø variablen (filstien) og giver den til den tomme variabel ovenfor
         _docPath = config["DocPath"];
+        _rabbitMQ = config["RabbitMQ"];
         _logger = logger;
+
         // Bruges til at skrive miljø variablen i konsolen
-        _logger.LogInformation($"Miljø variabel er sat til : {_docPath}");
+        _logger.LogInformation($"Fil sti er sat til : {_docPath}");
+        _logger.LogInformation($"RabbitMQ connection er sat til : {_rabbitMQ}");
     }
 
     public class PlanDTO
@@ -32,57 +36,57 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        var factory = new ConnectionFactory() { HostName = _rabbitMQ };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "Planning Service",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
+            _logger.LogInformation("Json object recieved at {DT}");
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
 
-            channel.QueueDeclare(queue: "Planning Service",
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
+            // Deserialize beskeden
+            var planningMessage = JsonConvert.DeserializeObject<PlanDTO>(message);
 
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) =>
+            //Bruger data fra POCO klasse til håndterbar data
+            Console.WriteLine(" [x] Received Planning Message: {0} - {1} (Due Date: {2})",
+                planningMessage.CustomerName,
+                planningMessage.PickupTime,
+                planningMessage.PickupLocation,
+                planningMessage.EndLocation);
+
+            // Instansieret PlanDTO C# object der kan bruges til test.
+            PlanDTO bobby = new PlanDTO
             {
-                _logger.LogInformation("Json object recieved at {DT}");
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-
-                // Deserialize beskeden
-                var planningMessage = JsonConvert.DeserializeObject<PlanDTO>(message);
-
-                //Bruger data fra POCO klasse til håndterbar data
-                Console.WriteLine(" [x] Received Planning Message: {0} - {1} (Due Date: {2})",
-                    planningMessage.CustomerName,
-                    planningMessage.PickupTime,
-                    planningMessage.PickupLocation,
-                    planningMessage.EndLocation);
-
-                // Instansieret PlanDTO C# object der kan bruges til test.
-                PlanDTO bobby = new PlanDTO
-                {
-                    CustomerName = "Steve",
-                    PickupTime = DateTime.Now,
-                    PickupLocation = "Aarhus",
-                    EndLocation = "Koebenhavn"
-                };
-
-                // Skriv til CSV fil metode. Husk at lave miljø variabel
-                // export DocPath="C:\Temp"
-                writeCSVFile(planningMessage);
-                
+                CustomerName = "Steve",
+                PickupTime = DateTime.Now,
+                PickupLocation = "Aarhus",
+                EndLocation = "Koebenhavn"
             };
 
-            channel.BasicConsume(queue: "Planning Service",
-                                 autoAck: true,
-                                 consumer: consumer);
+            // Skriv til CSV fil metode. Husk at lave miljø variabel
+            // export DocPath="C:\Temp"
+            writeCSVFile(planningMessage);
 
-            // Console.WriteLine(" Press [enter] to exit.");
-            // Console.ReadLine();
+        };
 
+        channel.BasicConsume(queue: "Planning Service",
+                             autoAck: true,
+                             consumer: consumer);
+
+        // Console.WriteLine(" Press [enter] to exit.");
+        // Console.ReadLine();
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             await Task.Delay(1000, stoppingToken);
         }
